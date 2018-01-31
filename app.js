204931b1d2e70606f3e3cb8c2c4f6ab65dca1830
kangxufeng <kangxufeng@duiba.com.cn>
2018-01-31 09:50:17 +0800

    fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var nunjucks = require('nunjucks');
-var axios = require('axios');
+var express = require('express');   // 引入express
+var nunjucks = require('nunjucks'); // 模板引擎
+var axios = require('axios');       // 网络请求
 var app = express();
 
-nunjucks.configure('./views', {
-  express: app
-});
+// 初始化模板引擎
+nunjucks.configure('./views', {express: app});
 app.set('view engine', 'html');
 
+// 路由处理
 app.get('/', function (req, res) {
   // 发送ajax请求
   axios({
@@ -19,10 +19,10 @@ app.get('/', function (req, res) {
       company: 'Duiba FED',
       banners: banners
     });
-  }).catch(err => {
-  });
+  })
 });
 
+// 启动web服务
 var server = app.listen(3000, function () {
   console.log('listening at http://localhost:3000');
 });
